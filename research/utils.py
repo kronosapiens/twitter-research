590c4913096930d48f3076d8bf215798193c86fc
Daniel Kronovet <kronovet@gmail.com>
2016-01-30 11:13:03 -0500

    Updated logging config to avoid saving tweet JSON

--- research/utils.py
+++ research/utils.py
@@ -1,6 +1,12 @@
 import logging
 
+import boto3
+
+FMT = '[%(name)s:%(levelname)s]:%(asctime)s:%(message)s'
+
 logging.basicConfig(
-    filename='stream.log', format='%(levelname)s:%(asctime)s:%(message)s', level=logging.INFO)
+    filename='stream.log', format=FMT, level=logging.INFO)
+
+boto3.set_stream_logger(name='boto3', level=logging.WARNING, format_string=FMT)
 
 # Note: if level is set to logging.DEBUG, boto3 prints full text of every putItem.
