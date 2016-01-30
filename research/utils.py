7f103d1e654e4d62b36d2710dd64180865c0b390
Daniel Kronovet <kronovet@gmail.com>
2016-01-30 10:56:59 -0500

    changed default log level from DEBUG to INFO

--- research/utils.py
+++ research/utils.py
@@ -1,4 +1,6 @@
 import logging
 
 logging.basicConfig(
-    filename='stream.log', format='%(levelname)s:%(asctime)s:%(message)s', level=logging.DEBUG)
+    filename='stream.log', format='%(levelname)s:%(asctime)s:%(message)s', level=logging.INFO)
+
+# Note: if level is set to logging.DEBUG, boto3 prints full text of every putItem.
