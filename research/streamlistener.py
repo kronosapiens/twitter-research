960dee50fb1787c8ad83e89b84402577a3c80153
Daniel Kronovet <kronovet@gmail.com>
2016-01-30 11:30:12 -0500

    updated tweet to string formatting

--- research/streamlistener.py
+++ research/streamlistener.py
@@ -15,7 +15,7 @@ from utils import logging
 class MyStreamListener(StreamListener):
     def __init__(self, storage='stdout'):
         if storage == 'nosql':
-            dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+            dynamodb = boto3.resource('dynamodb')
             self.tweets = dynamodb.Table('tweets')
         elif storage == 'sql':
             self.conn = db.engine.connect()
@@ -44,11 +44,7 @@ class MyStreamListener(StreamListener):
 
     def on_status(self, status):
         if self.storage == 'stdout':
-            print self.to_string(
-                status.author.screen_name,
-                status.created_at,
-                status.text,
-            )
+            print self.status_to_string(status)
         elif self.storage == 'sql':
             try:
                 self.to_sql(status)
@@ -74,37 +70,20 @@ class MyStreamListener(StreamListener):
     ####################
     ### OUTPUT FUNCTIONS
 
+    def status_to_string(self, status):
+        return self.to_string(
+            status.author.screen_name,
+            status.created_at,
+            status.text
+        )
+
     def to_string(self, screen_name, created_at, text):
-        string = u'[{}-{}] {}'.format(screen_name, created_at, text)
+        string = u'{} [ {} ] {}'.format(created_at, screen_name, text)
         return string.encode('utf-8')
 
     def to_json(self, raw_data):
         tweet_dict = json.loads(raw_data, parse_float=decimal.Decimal)
-
         self.remove_empty_strings(tweet_dict)
-
-        # sub_tweets = [
-        #     tweet_dict,
-        #     tweet_dict.get('retweeted_status', {}),
-        #     tweet_dict.get('retweeted_status', {}).get('quoted_status', {}),
-        #     tweet_dict.get('quoted_status', {}),
-        #     tweet_dict.get('quoted_status', {}).get('retweeted_status', {}),
-        # ]
-
-        # sub_tweets = [st for st in sub_tweets if st] # Skip empty dicts
-
-        # keys = [
-        #     'profile_background_image_url',
-        #     'profile_background_image_url_https'
-        # ]
-
-        # for sub_tweet in sub_tweets:
-        #     if sub_tweet.get('source') == '':
-        #         del sub_tweet['source']
-        #     for key in keys:
-        #         if sub_tweet.get('user', {}).get(key, None) == '':
-        #             del sub_tweet['user'][key]
-
         return tweet_dict
 
     def to_sql(self, status):
@@ -114,7 +93,7 @@ class MyStreamListener(StreamListener):
             user_location=status.author.location,
             created_at=status.created_at,
             text=status.text
-            )
+        )
         self.conn.execute(ins)
 
     def to_nosql(self, tweet_dict):
