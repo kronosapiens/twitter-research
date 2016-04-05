1601e8fd9f1e991346b3441a3ab45a5a7f37731e
Daniel Kronovet <kronovet@gmail.com>
2016-04-05 16:34:47 -0400

    updated generate_csv to generate summary csv files

--- research/config.py
+++ research/config.py
@@ -9,4 +9,5 @@ DATA_DIR = 'data'
 JSON_BUCKET = 'primary-tweets'
 SUMMARY_BUCKET = 'primary-tweets-summaries'
 CSV_BUCKET = 'primary-tweets-csv'
+SUMMARY_CSV_BUCKET = 'primary-tweets-summaries-csv'
 SUMMARIZE_FREQUENCY = 10 # 1/10 lines kept
--- research/generate_csv.py
+++ research/generate_csv.py
@@ -20,6 +20,7 @@ from to_csv import to_csv
 FILE_TEMPLATE = 'tweets.{}.json'
 CSV_TEMPLATE = 'tweets.{}.csv'
 SUMMARY_TEMPLATE = 'tweets.{}.summary.json'
+SUMMARY_CSV_TEMPLATE = 'tweets.{}.summary.csv'
 
 def upload_file(file_name, path, bucket):
     full_path = '{}/{}'.format(path, file_name)
@@ -30,45 +31,23 @@ path_root = os.path.dirname(os.path.abspath(__file__))
 path = path_root + '/../' + config.DATA_DIR
 
 s3 = boto3.resource('s3')
-matcher = re.compile('tweets[\d.]+json') # Skip summary files
 
-raw_json_filenames = [
-    obj.key for obj in s3.Bucket(config.JSON_BUCKET).objects.all()
-    if matcher.match(obj.key)
-]
+# matcher = re.compile('tweets[\d.]+json') # Skip summary files
+# raw_json_filenames = [
+#     obj.key for obj in s3.Bucket(config.JSON_BUCKET).objects.all()
+#     if matcher.match(obj.key)
+# ]
 
-for file_name in raw_json_filenames:
 
-    date_to_copy = file_name.split('tweets.')[1].split('.json')[0]
+for obj in s3.Bucket(config.SUMMARY_BUCKET).objects.all():
+    file_name = obj.key
+    date_to_copy = file_name.split('tweets.')[1].split('.summary.json')[0]
 
     print 'Downloading', file_name
-    s3.Bucket(config.JSON_BUCKET).download_file(file_name, '{}/{}'.format(path, file_name))
-
-    print 'Generating summary of', file_name, '...'
-    summary_name = SUMMARY_TEMPLATE.format(date_to_copy)
-    cmd = "awk 'NR == 1 || NR % {freq} == 0' {path}/{source} > {path}/{dest}"
-    cmd = cmd.format(freq=config.SUMMARIZE_FREQUENCY, path=path, source=file_name, dest=summary_name)
-
-    try:
-        subprocess.call(cmd, shell=True)
-        print 'Summary created!'
-    except OSError as ex:
-        logging.warning(ex)
-        print 'Summary creation failed for reason:', ex
-
-    try:
-        upload_file(summary_name, path, config.SUMMARY_BUCKET)
-        print 'Summary copied!'
-    except OSError as ex:
-        logging.warning(ex)
-        print 'Summary copy failed for reason:', ex
-    else:
-        os.remove('{}/{}'.format(path, summary_name))
-        print 'Sumary deleted!'
-
+    s3.Bucket(config.SUMMARY_BUCKET).download_file(file_name, '{}/{}'.format(path, file_name))
 
     print 'Generating CSV of', file_name, '...'
-    csv_name = CSV_TEMPLATE.format(date_to_copy)
+    csv_name = SUMMARY_CSV_TEMPLATE.format(date_to_copy)
     try:
         to_csv('{}/{}'.format(path, file_name), '{}/{}'.format(path, csv_name), level=3)
         print 'CSV created!'
@@ -77,7 +56,7 @@ for file_name in raw_json_filenames:
         print 'CSV creation failed for reason:', ex
 
     try:
-        upload_file(csv_name, path, config.CSV_BUCKET)
+        upload_file(csv_name, path, config.SUMMARY_CSV_BUCKET)
         print 'CSV copied!'
     except OSError as ex:
         logging.warning(ex)
