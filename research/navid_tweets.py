11597167679740b6ebf5477d4f6b8bbbab94d89c
Daniel Kronovet <kronovet@gmail.com>
2016-04-17 18:45:12 -0400

    wrote script to parse navids tweets

---
+++ research/navid_tweets.py
@@ -0,0 +1,31 @@
+'''
+Daniel Kronovet
+dbk2123@columbia.edu
+
+Code snippets for handling Navid's string escape tweet files.
+'''
+
+# To download:
+# wget -O file.tar "http://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE46130&format=file"
+
+import json
+
+def clean_tweets(input_file, output_file):
+    with open(input_file) as f:
+        with open(output_file) as output:
+        tweets = f.read().decode('string_escape').split('\n')
+        for t in tweets:
+            try:
+                tweet = json.loads(t[2:])
+                tweet = json.dumps(tweet)
+                output.write(tweet + '\n')
+            except ValueError as ex:
+                print ex
+
+if __name__ == '__main__':
+    import sys
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    clean_tweets(input_file, output_file)
+
+
